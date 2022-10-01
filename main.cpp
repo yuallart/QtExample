e39d01c39d8ad268c913f1191a8e7b3d52664447
yuallart <78012935+yuallart@users.noreply.github.com>
2022-10-02 00:11:15 +0800

    init widgets

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("QT Example");
     w.show();
-
     return QApplication::exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,14 +2,15 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+        QMainWindow(parent),
+        ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    QString imageLocation = *new QString(":/resource/images/image.jpg");
+    ui->label->setPixmap(QPixmap(imageLocation));
+    ui->label->setScaledContents(true);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
